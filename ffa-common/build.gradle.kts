f6a9e4a15415ccbfdbc5ab8a1ee544c74b7de3dc
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2024-05-30 09:32:26 +0200

    feat: added maven publish <https://github.com/HGLabor/hero-modules/issues/1>

--- ffa-common/build.gradle.kts
+++ ffa-common/build.gradle.kts
@@ -6,9 +6,11 @@ plugins {
     kotlin
     silk
     kotlin("plugin.serialization")
+    `maven-publish`
 }
 
-version = "1.0.0"
+val mcVersion = "1.20.4"
+version = "$mcVersion-1.0.0"
 
 repositories {
     mavenCentral()
@@ -31,3 +33,38 @@ tasks {
         }
     }
 }
+
+
+val sourceJar = tasks.register<Jar>("sourceJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "gg.norisk"
+            artifactId = "ffa-common"
+            version = project.version.toString()
+            from(components["java"])
+            artifact(sourceJar)
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://maven.norisk.gg/repository/maven-releases/")
+            val snapshotsRepoUrl = uri("https://maven.norisk.gg/repository/maven-snapshots/")
+            val nexusUsername = System.getenv("NORISK_NEXUS_USERNAME") ?: project.findProperty("noriskMavenUsername") ?: ""
+            val nexusPassword = System.getenv("NORISK_NEXUS_PASSWORD") ?: project.findProperty("noriskMavenPassword") ?: ""
+
+            name = "nexus"
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = nexusUsername.toString()
+                password = nexusPassword.toString()
+            }
+        }
+    }
+}
+
+
--- ffa-server/build.gradle.kts
+++ ffa-server/build.gradle.kts
@@ -5,9 +5,11 @@ plugins {
     fabric
     kotlin
     silk
+    `maven-publish`
 }
 
-version = "1.0.0"
+val mcVersion = "1.20.4"
+version = "$mcVersion-1.0.3"
 
 repositories {
     mavenCentral()
@@ -40,5 +42,38 @@ tasks {
         }
     }
 }
+
+val sourceJar = tasks.register<Jar>("sourceJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "gg.norisk"
+            artifactId = "ffa-server"
+            version = project.version.toString()
+            from(components["java"])
+            artifact(sourceJar)
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://maven.norisk.gg/repository/maven-releases/")
+            val snapshotsRepoUrl = uri("https://maven.norisk.gg/repository/maven-snapshots/")
+            val nexusUsername = System.getenv("NORISK_NEXUS_USERNAME") ?: project.findProperty("noriskMavenUsername") ?: ""
+            val nexusPassword = System.getenv("NORISK_NEXUS_PASSWORD") ?: project.findProperty("noriskMavenPassword") ?: ""
+
+            name = "nexus"
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = nexusUsername.toString()
+                password = nexusPassword.toString()
+            }
+        }
+    }
+}
+
 
 
